1e64bd459c106a164ab5c2c8d8bc35221916e8ff
Nikhil Korrapati <nikhilkorrapati>
2025-06-11 20:31:14 -0400

    Updated Security Version with the new Auth0

---
+++ OrderService/src/main/java/com/nikhilspring/OrderService/service/TokenService.java
@@ -0,0 +1,26 @@
+package com.nikhilspring.OrderService.service;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Service
+public class TokenService {
+
+    public String extractToken() {
+        ServletRequestAttributes attributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            String authHeader = request.getHeader("Authorization");
+
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                return authHeader.substring(7);  // Extract token value
+            }
+        }
+        return null;
+    }
+}
